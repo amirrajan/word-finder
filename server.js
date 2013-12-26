f2e5a8c36ccd2dc3e3e3a8fc750d0697b7e64284
Barry Forrest <bforrest30@gmail.com>
2013-12-26 08:59:37 -0600

    removed "front matter" that was not needed

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var _ = require('underscore');
 var app = express();
-//var server = require('http').createServer(app);
-var parseCookie = require('./lib/cookie_parser');
+
 var config = require('./lib/config');
 var words = require('./lib/words');
 
@@ -10,10 +9,7 @@ app.set('view engine', 'ejs');
 app.set('view options', { layout: false });
 app.use('/public', express.static('public'));
 
-app.use(express.methodOverride());
 app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.session({secret: 'sooo secret'}));
 
 app.use(app.router);
 
@@ -26,6 +22,5 @@ app.post('/search', function (req, res) {
   res.render('result', { words: result, pattern: req.body.pattern });
 });
 
-//server.listen(process.env.PORT || config.port);
 app.listen(process.env.PORT || config.port);
 console.log("Listening on port: " + (process.env.PORT || config.port));
