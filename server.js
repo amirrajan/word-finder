6d18ef7791b24d00a157f43488eb4cedce7baf73
JustinJohnWilliams <justinjohnwilliams@gmail.com>
2016-04-30 14:43:15 -0500

    does it work?

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
+require('babel-core/register');
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('lodash');
 var app = express();
 
-var config = require('./lib/config');
 var words = require('./lib/words');
-
 app.set('view engine', 'ejs');
 app.set('view options', { layout: false });
 app.use('/public', express.static('public'));
@@ -22,5 +21,5 @@ app.post('/search', function (req, res) {
   res.render('result', { words: result, pattern: req.body.pattern });
 });
 
-app.listen(process.env.PORT || config.port);
-console.log("Listening on port: " + (process.env.PORT || config.port));
+app.listen(process.env.PORT || 3000);
+console.log("Listening on port: " + (process.env.PORT || 3000));
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+var webpack = require('webpack');
+var path = require('path');
+var fs = require('fs');
+
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  entry: './server.js',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'backend.js'
+  },
+  externals: nodeModules
+};
